0047728655cc08549632807fde8fefde66d5921b
Mugsy Carter <mugsycarter@gmail.com>
2016-10-27 17:38:22 -0700

    Project ruBasic features up and running on home page load the HTML and JSON data are appended to the page.  User data is collected with a form.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a single resourse http server.
+
+It allows users to enter world capitals in the page and also retrieve a persistent list of world capitals.
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const server = require('./server');
-const router = require('./router');
-
+const server = require('./lib/server');
+// const promise = require('./lib/promise');
+// const fs = require('fs');
+// const http = require('http');
+// const indexHtml = fs.createReadStream('index.html');
+const router = require('./lib/router');
 
 server.start(router.route);
